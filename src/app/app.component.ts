a24520cb1798d631e2d815b8b355e6115b244de8
Kasim01~ <shaikhkasim218@gmail.com>
2023-12-19 19:46:30 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  selectedServiceType: string = ''; // Add this property to track the selected service type
+
+  confirmTrack() {
+    // Implement your logic for confirmTrack
+    throw new Error('Method not implemented.');
+  }
+
+  verifyCaptcha() {
+    // Implement your logic for verifyCaptcha
+    throw new Error('Method not implemented.');
+  }
+
   title = 'client';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { ServicerequestComponent } from './servicerequest/servicerequest.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicerequestComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule
   ],
   providers: [
     provideClientHydration()
