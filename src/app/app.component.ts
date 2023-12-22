34131fc650c3a77f12dfef31fdd44b5108c68bd3
Kasim01~ <shaikhkasim218@gmail.com>
2023-12-22 19:00:32 +0530

    final change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
-import { SidenavStateService } from './shared/services/sidenav-state.service';
 
 @Component({
   selector: 'app-root',
@@ -11,32 +10,12 @@ export class AppComponent {
   isSidenavFadedOut = false;
 
   constructor(private renderer: Renderer2,
-    private el: ElementRef,
-    private sidenavStateService: SidenavStateService
+    private el: ElementRef
   ) { }
 
   ngOnInit(): void {
-    this.handleWindowLoad();
-
-    // Subscribe to the sidenav state observable
-    this.sidenavStateService.isSidenavFadedOut$.subscribe((fadedOut) => {
-      this.isSidenavFadedOut = fadedOut;
-    });
   }
 
   handleWindowLoad(): void {
-    const preloader = this.el.nativeElement.querySelector('.preloader') as HTMLElement;
-
-    // Hide preloader
-    if (preloader) {
-      this.renderer.setStyle(preloader, 'display', 'none');
-
-      // Additional setTimeout logic (if needed)
-      setTimeout(() => {
-        this.renderer.setStyle(preloader, 'display', 'none');
-      }, 1500);
-    } else {
-      console.error('Preloader element not found');
-    }
   }
 }
