af2ddcf43bf34456769eb20ceafe6531183ef1d6
Kasim01~ <shaikhkasim218@gmail.com>
2023-12-27 18:40:16 +0530

    implemented captcha

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,5 +1,4 @@
-// registration.component.ts
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, Renderer2, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
@@ -7,7 +6,7 @@ import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms'
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements AfterViewInit {
   name: string = '';
   email: string = '';
   mobile: string = '';
@@ -15,10 +14,12 @@ export class RegistrationComponent {
   password: string = '';
   confirmPassword: string = '';
   captcha: string = '';
-
+  userEnteredCaptcha: string = '';
   registrationForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  @ViewChild('captchaCanvas', { static: false }) captchaCanvas!: ElementRef<HTMLCanvasElement>;
+
+  constructor(private fb: FormBuilder, private renderer: Renderer2) {
     this.registrationForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
@@ -28,6 +29,35 @@ export class RegistrationComponent {
       confirmPassword: ['', Validators.required],
       captcha: ['', Validators.required],
     });
+    this.generateCaptcha();
+  }
+
+  ngAfterViewInit(): void {
+    this.drawCaptcha();
+  }
+
+  get captchaControl(): FormControl {
+    return this.registrationForm.get('captcha') as FormControl;
+  }
+
+  generateCaptcha(): void {
+    this.captcha = this.generateRandomString(6);
+  }
+
+  generateRandomString(length: number): string {
+    const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      result += charset[randomIndex];
+    }
+    return result;
+  }
+
+  refreshCaptcha(): void {
+    this.generateCaptcha();
+    this.userEnteredCaptcha = '';
+    this.drawCaptcha();
   }
 
   upper(value: string): void {
@@ -35,7 +65,29 @@ export class RegistrationComponent {
   }
 
   register(): void {
-    // Your registration logic here
-    // Access form values using this.registrationForm.value
+    if (this.userEnteredCaptcha.toUpperCase() === this.captcha.toUpperCase()) {
+      console.log('Captcha matched. Proceed with registration.');
+    } else {
+      console.log('Invalid captcha. Registration failed.');
+    }
+  }
+
+  drawCaptcha(): void {
+    const canvas = this.captchaCanvas.nativeElement;
+    const ctx = canvas.getContext('2d');
+
+    console.log(canvas); // Check if the canvas is found
+    console.log(ctx);    // Check if the context is available
+    
+    if (ctx) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.font = '20px Arial';
+      ctx.fillStyle = 'black';
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      ctx.fillText(this.captcha, canvas.width / 2, canvas.height / 2);
+    } else {
+      console.error('Canvas context not available');
+    }
   }
 }
